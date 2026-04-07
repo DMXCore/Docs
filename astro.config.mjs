1332f8dfa32621485ae67ae20a247c66b7b6bff1
Hakan Lindestaf <hakan@lindestaf.com>
2026-04-07 14:15:18 -0500

    Added GTM

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,20 @@ export default defineConfig({
         }),
       ],
       customCss: ['./src/styles/custom.css'],
-      head: [],
+      head: [
+        {
+          tag: 'script',
+          content: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W7MVJVZS');`,
+        },
+        {
+          tag: 'noscript',
+          content: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W7MVJVZS" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        },
+      ],
       sidebar: [
         {
           label: 'DMX Core 100',
